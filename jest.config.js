49da87865b5f9ec371380990a7a6df93b2196f9d
Steffen Krohn Storgaard <skrohnstorgaard@gmail.com>
2022-01-24 21:56:31 +0100

    fix ts

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,13 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   transform: {
-    '^.+\\.(t)sx?$': [
+    '^.+\\.[jt]sx?$': [
       '@swc/jest',
       {
         jsc: {
           parser: {
             syntax: 'typescript',
             tsx: true
-          },
-          transform: {
-            react: {
-              runtime: 'automatic'
-            }
-          }
-        }
-      }
-    ],
-    '^.+\\.(j)sx?$': [
-      '@swc/jest',
-      {
-        jsc: {
-          parser: {
-            syntax: 'ecmascript',
-            jsx: true
           },
           transform: {
             react: {
