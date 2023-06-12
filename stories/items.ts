a1ab0538e3fcfc12e8bc6b746ab8db32ac9d7d87
vembye <frederik@vembyte.dk>
2023-06-12 16:30:46 +0200

    Seed test data

---
+++ stories/items.ts
@@ -0,0 +1,17 @@
+export interface IItem {
+  name: string;
+  category: string;
+  price: string;
+  stocked: boolean;
+}
+
+const items: IItem[] = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
+export default items;
