5650c9fc759a44e4ef5d9ff023bee39166512533
vembye <frederik@vembyte.dk>
2024-12-02 20:03:15 +0100

    Add stock filter

--- stories/FilterableProductTable.tsx
+++ stories/FilterableProductTable.tsx
@@ -1,20 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ProductTable } from "./ProductTable";
-import items from "./items";
+import { IItem } from "./items";
 import { SearchBar } from "./SearchBar";
+
+function useItems(filter: string): {
+  isLoading: boolean;
+  items: IItem[];
+  error;
+} {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  async function getAndSetData() {
+    setIsLoading(true);
+    const response = await fetch(
+      `https://raw.githubusercontent.com/vembytex/workshops/introduction-to-react/tasks/items.json?filter=${filter}`
+    );
+    const result = await response.json();
+
+    if (!response.ok)
+      throw new Error("Requst failed with status code" + response.status);
+
+    setItems(result);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    getAndSetData();
+  }, []);
+
+  return { items, error, isLoading };
+}
 
 export function FilterableProductTable() {
   const [searchValue, setSearchValue] = useState("");
+  const [showInStockOnly, setShowInStockOnly] = useState(false);
+  const { items, error, isLoading } = useItems(`search=${searchValue}`);
 
-  const itemsToRender = items.filter((item) =>
-    item.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase())
+  const itemsToRender = items.filter(
+    (item) =>
+      item.name.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()) &&
+      (!showInStockOnly || item.stocked)
   );
+
   return (
     <div>
       <SearchBar
         searchValue={searchValue}
-        onSearchValueChange={(value) => setSearchValue(value)}
+        stockValue={showInStockOnly}
+        onStockValueChange={setShowInStockOnly}
+        onSearchValueChange={setSearchValue}
       />
+      {isLoading && "Loading results..."}
+      {error}
       <ProductTable items={itemsToRender} />
     </div>
   );
--- stories/SearchBar.tsx
+++ stories/SearchBar.tsx
@@ -2,7 +2,9 @@ import React from "react";
 
 export interface ISearchBarProps {
   searchValue: string;
+  stockValue: boolean;
   onSearchValueChange: (value: string) => void;
+  onStockValueChange: (value: boolean) => void;
 }
 
 export function SearchBar(props: ISearchBarProps) {
@@ -15,7 +17,12 @@ export function SearchBar(props: ISearchBarProps) {
       />
       <div>
         <label>
-          <input type="checkbox" /> Only show products in stock
+          <input
+            type="checkbox"
+            checked={props.stockValue}
+            onChange={(e) => props.onStockValueChange(!props.stockValue)}
+          />{" "}
+          Only show products in stock
         </label>
       </div>
     </div>
